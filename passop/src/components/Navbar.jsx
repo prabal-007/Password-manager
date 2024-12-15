a155e3f77857e9983908f86c01d8900065651a81
Starkk <42775517+prabal-007@users.noreply.github.com>
2024-12-15 18:16:33 +0530

    added navbar

---
+++ passop/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+
+export const Navbar = () => {
+  return (
+    <nav className="flex justify-around">
+        <div className="text-xl font-bold">PassOP</div>
+        <ul className="flex gap-10">
+            <li>Home</li>
+            <li>About</li>
+            <li>Conatct</li>
+        </ul>
+    </nav>
+  )
+}
